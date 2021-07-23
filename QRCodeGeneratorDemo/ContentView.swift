d072ec331906f593cda0fcc516c1f51ac5f6fdb2
Wolf McNally <wolf@wolfmcnally.com>
2021-07-23 00:57:09 -0700

    Initial Commit

---
+++ QRCodeGeneratorDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRCodeGeneratorDemo
+//
+//  Created by Wolf McNally on 7/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QRCodeGeneratorDemo/QRCodeGeneratorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGeneratorDemoApp.swift
+//  QRCodeGeneratorDemo
+//
+//  Created by Wolf McNally on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGeneratorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
